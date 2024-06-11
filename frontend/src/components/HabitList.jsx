55754ad42d77b46f43af6bd136a6826845d0ae34
ritikpatidar20 <patidaritik2000@gmail.com>
2024-06-11 18:40:22 +0530

    Add basic structure and routing for React app

---
+++ frontend/src/components/HabitList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HabitList = () => {
+    return (
+        <div>
+            <h2>Your Habits</h2>
+            {/* Habits will be listed here */}
+        </div>
+    );
+};
+
+export default HabitList;
